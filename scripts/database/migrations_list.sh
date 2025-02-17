207f73736f5856bccd1eec5d837e15ff41d78e0e
Greg Detre <greg@gregdetre.co.uk>
2025-02-17 02:25:07 +0000

    Removed duplicate scripts
    that somehow got returned when we did the Git fixing

--- scripts/database/migrations_list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# List migrations for local development database
-POSTGRES_DB_NAME="hellozenno_development" python utils/migrate.py list 
--- scripts/run_local_flask_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-FLASK_DEBUG=1 FLASK_APP=app.py flask run --host=localhost --port 3000
