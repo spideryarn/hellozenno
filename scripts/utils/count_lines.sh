e5c1180e6e7ddc4d47a7db715599c5395e0e7a74
Greg Detre <greg@gregdetre.com>
2025-03-22 19:58:10 +0000

    Count lines by file

--- scripts/utils/count_lines.sh
+++ scripts/utils/count_lines.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# --by-file \
 cloc . \
-    --exclude-dir=gjdutils,.pytest_cache,__pycache__,.vscode,backup,data,credentials,HelloZenno \
-    --exclude-ext=md \
-    --not-match-f='.*\.md$' \
+    --exclude-dir=gjdutils,.pytest_cache,__pycache__,.vscode,backup,data,credentials,package-lock.json,HelloZenno,node_modules,extern,dist,.svelte-kit,build,.vite,logs,screenshots,.cursor,obsolete,.vercel \
+    --exclude-ext=md,pyc,pyo,pyd,log,png \
+    --not-match-f='.*\.md$|_secrets\.py|\.env\..*|npm-debug\.log.*|yarn-debug\.log.*|yarn-error\.log.*|\.pnpm-debug\.log.*' \
     static/css 
